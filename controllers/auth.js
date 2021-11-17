4585bf7ff61cf773466112692740ce54ba8d7cd1
Debanjan Tewary <92116204+misvecinosriju@users.noreply.github.com>
2021-11-17 14:28:59 +0530

    adding login function with jwt token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 import User from "../models/user";
 import { hashPassword, comparePassword } from "../utils/auth";
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 export const register = async (req, res) => {
   try {
@@ -28,5 +30,38 @@ export const register = async (req, res) => {
   } catch (err) {
     console.log(err);
     return res.status(400).send("Opps. Something wrong with api try again");
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //check user is valid
+    const user = await User.findOne({ email }).exec();
+    if (!user) return res.status(400).send("No user found with this email");
+
+    //if user found then comapre password
+    const passwordMatch = await comparePassword(password, user.password);
+    if (!passwordMatch)
+      return res.status(400).send("Please Enter correct Password");
+    //generate token
+    const token = jwt.sign(
+      { _id: user._id },
+      "SDYBUDSHFS7NH87EY3Q7SEZQNS78ZZQDNP78YN73ZYQ478S3Q4NNQPS78E78Y37QSNOZQ78FYQSZD77Q3NP7CQDHQ0Q9QDQ0FS7N8347",
+      {
+        expiresIn: "7d",
+      }
+    );
+    //send cookie without user's password
+    user.password = undefined;
+    res.cookie("token", token, {
+      httpOnly: true,
+    });
+    //send data to user frontend
+    res.json(user);
+  } catch (err) {
+    console.log("Error", err);
+    res.status(400).send("Error with Login");
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 import express from "express";
 //importing controllers to control routes callback , for maintainable code
-import { register } from "../controllers/auth";
+import { register, login } from "../controllers/auth";
 
 //creating router instance like app
 const router = express.Router();
 
 //register user endpoint
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
