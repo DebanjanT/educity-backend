836ec7518774dc75d3bafc2871879df6f8e220f1
Debanjan Tewary <92116204+misvecinosriju@users.noreply.github.com>
2021-11-22 15:15:53 +0530

    csrf route added

--- server.js
+++ server.js
@@ -4,18 +4,32 @@ const morgan = require("morgan");
 require("dotenv").config();
 import { readdirSync } from "fs";
 import mongoose from "mongoose";
+import csrf from "csurf";
+import cookieParser from "cookie-parser";
 
 // ** create express app
 const app = express();
+
+//csrf protection
+const csrfProtection = csrf({ cookie: true });
 
 // ** express middleware (execute before sending response to client)
 app.use(express.json());
 app.use(cors());
 //morgan for debug requestsß
 app.use(morgan("dev"));
+app.use(cookieParser());
 
 // ** routes using fileSystem of node by for loop after reading router folder
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+
+//csrf middleware
+app.use(csrfProtection);
+
+//csrf API endpoint to recieve CSRF from frontend
+app.get("/api/csrf-token", (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
+});
 
 //db connect with atlas by mongoose
 mongoose
